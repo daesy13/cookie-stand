1e2d15a03c2896bedbbe0edbef96faee7a20243d
daesy <daesyramos@hotmail.com>
2019-11-12 23:49:34 -0800

    added all cities

--- js/testapp.js
+++ js/testapp.js
@@ -3,6 +3,18 @@
 // Global Variables
 var container = document.getElementById('stores');
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+// var overallTotal = 0;
+// var storeArr=[];
+
+// var hourlyObj = new Object();
+// for (var i = 0; i < hours.length; i++) {
+//   hourlyObj[hours[i]];
+// }
+// for (var i = 0; i < hours.length; i++) {
+//   hourlyObj.key = hours[i];
+// }
+
+
 
 // Global Random Function
 function randomNumCust (min, max, avrg) {
@@ -34,6 +46,8 @@ function Store (name, minCustomer, maxCustomer, avrgCookieSales) {
   }
 }
 
+
+
 // Creates a container
 var article = addElement('article', container);
 
@@ -62,29 +76,79 @@ Store.prototype.render = function(container) {
     addElement('td', dataRowElem, this.cookiesSold[i]);
     // hourlyObj.hours[i].push(this.cookiesSold[i]);
   }
-  
+  // console.log('dataRowElem: ', dataRowElem.nth-child(2));
+  // document.querySelector("#stores > article > table > tr:nth-child(2) > td:nth-child(2)")
   // ***** This add total as last columm
   addElement('td', dataRowElem, this.total);
 
+
 }
 
+// console.log(cookiesSold)
 var storeContainer = document.getElementById('stores');
 
 var allStores = [];
 
-// UPLOADING EACH STORE
 allStores.push(new Store('Seattle',23,65,6.3));
 allStores.push(new Store('Tokyo',3,4,1.2));
-allStores.push(new Store('Dubai', 11,	38,	3.7));
-allStores.push(new Store('Paris',	20,	38,	2.3));
-allStores.push(new Store('Lima', 2,	16,	4.6));
+// allStores.push(new Store();
 
+// Total Per Hour
+var totalHourArr = [];
+var totalPerHour = 0;
 
-// Iterating and rendering each store
+// Iterates each row
 for(var i = 0; i < allStores.length; i++) {
   var eachStore = allStores[i];
   eachStore.render(storeContainer);
 }
+
+
+
+// console.log('allStores: ', allStores)
+// #stores > article > table > tr:nth-child(2) > td:nth-child(2)
+// #stores > article > table > tr:nth-child(3) > td:nth-child(2)
+
+// #stores > article > table > tr:nth-child(2) > td:nth-child(3)
+// #stores > article > table > tr:nth-child(3) > td:nth-child(3)
+
+// #stores > article > table > tr:nth-child(2) > td:nth-child(16)
+
+// function hourlyTotal(hrsArr, salesArr) {
+//   for (var i = 0; i < hours.length; i++) {
+//     hourlyObj[hours[i]];
+
+//   var dictHourlyTotal = []; // create an empty array
+//   for(var j = 2; j < 17; j++){
+//     dictHourlyTotal.push({
+//       key: hours[j],
+//       value: document.querySelector("#stores > article > table >tr:nth-child(3)[j]")
+//     })
+//   }
+// }
+
+// // console.log('dictHourlyTotal: ', dictHourlyTotal(hourlyTotal))
+
+
+// for(var j=2; j<17; j++){
+//   console.log('hours[i]: ', this.cookiesSold.nth-child(j));
+  
+//   // hourlyObj[hours[j]] = this.cookiesSold[j];
+// }
+  // console.log('eachStore.cookiesSold[i]: ', eachStore.cookiesSold[i])
+  // console.log('hourlyObj: ', hourlyObj)
+// console.log('hourlyObj: ', hourlyObj)
+
+// CREATING OBJECT FOR EVERY HOUR STORES TOTAL
+
+
+// sumVal = document.getElementById("val");
+// console.log('sumVal: ', sumVal);
+
+// console.log('hourlyObj: ', hourlyObj)
+
+// key = hours
+// value = cookiesSold[i]
 
 // *****ADD FOOTER TABLE WITH TOTAL COOKIES OF ALL STORES****************
 // FOOTER
@@ -94,7 +158,10 @@ var dataRowElemm2 = addElement('tr', tableElem);
 
 addElement('td', dataRowElemm2, 'Totals');
 
-// NEED TO ADD 15 TOTALS OF COLUMN SUM the dollar sign is a place holder
+// NEED TO ADD 15 TOTALS OF COLUMN SUM
 for (var i = 0; i < 15; i++) {
-  addElement('td', dataRowElemm2, '$');
+  addElement('td', dataRowElemm2, totalPerHour[i]);
 }
+
+document.querySelector("#stores > article > table > tr:nth-child(3) > td:nth-child(2)")
+document.querySelector("#stores > article > table > tr:nth-child(3) > td:nth-child(2)")
